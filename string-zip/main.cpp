82145927eb5dac5fd6b47c494586a3c1f56a42f2
fangxin0709 <167990114+fangxin0709@users.noreply.github.com>
2026-01-13 22:05:19 +0800

    Update main.cpp

--- string-zip/main.cpp
+++ string-zip/main.cpp
@@ -23,4 +23,37 @@ int main() {
 	}
     return 0;
 }
+using namespace std;
+
+// int main() {
+//     int n;
+//     cin >> n;
+
+//     // 用兩層迴圈跑每一格
+//     for (int i = 0; i < n; i++) {
+//         for (int j = 0; j < n; j++) {
+
+//             // 算離四個邊的距離
+//             int top = i;
+//             int left = j;
+//             int bottom = n - 1 - i;
+//             int right = n - 1 - j;
+
+//             // 找最小距離
+//             int minDist = top;
+//             if (left < minDist) minDist = left;
+//             if (bottom < minDist) minDist = bottom;
+//             if (right < minDist) minDist = right;
+
+//             // 數字 = 最小距離 + 1
+//             cout << minDist + 1;
+
+//             // 空格（最後一個不用）
+//             if (j != n - 1) cout << " ";
+//         }
+//         cout << endl;
+//     }
+
+//     return 0;
+// }
 
